0ab17ba36a56df0648cfd7103e70bbfa861a356a
Дмитрий <melehan@mail.ru>
2024-05-24 12:07:33 +0300

    Update homework5.py
    Исправлены ошибки

--- homework5.py
+++ homework5.py
@@ -1,8 +1,8 @@
 my_list = ["яблоко", "банан", "киви", "лимон", "апельсин"]
 print(my_list)
 print(my_list[0], my_list[3])
-print(my_list[3:5])
-my_list[3] = "вишня"
+print(my_list[2:5]) # чистая не внимательность, нет оправданий!
+my_list[2] = "вишня" # то же самое
 print(my_list)
 my_dict ={'orange': 'апельсин', 'pear':	'груша', 'apple': 'яблоко'}
 print(my_dict)
